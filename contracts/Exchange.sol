897774b6e3b42f6bb9783a3bf9e6b2caf2ce8dd1
Danny <DJ@MacBook-Pro.local>
2018-03-03 19:39:37 -0700

    contracts added

---
+++ contracts/Exchange.sol
@@ -0,0 +1,137 @@
+pragma solidity ^0.4.0;
+
+import "./owned.sol";
+import "./ERC20Standard.sol";
+
+contract Exchange is owned {
+    
+    struct Offer {
+        uint amount;
+        address who;
+        
+    }
+    
+    struct OrderBook {
+        uint higherPrice;
+        uint lowerPrice;
+        
+        mapping(uint => Offer) offers;
+        
+        uint offers_key;
+        uint offers_length;
+        
+    }
+    
+    struct Token {
+        address tokenContract;
+        string symbolName;
+        
+        mapping(uint => OrderBook) buyBook;
+        uint curBuyPrice;
+        uint lowestBuyPrice;
+        uint amountBuyPrices;
+        
+        mapping(uint => OrderBook) sellBook;
+        uint curSellPrice;
+        uint lowestSellPrice;
+        uint amountSellPrices;
+        
+    }
+    
+    mapping (uint8 => Token) tokens;
+    
+    uint8 symbolNameIndex;
+    
+    //Balances//
+    
+    mapping(address => mapping(uint8 => uint)) tokenBalanceForAddress;
+    
+    mapping(address => uint) balanceEthForAddresses;
+    
+    //Events//
+    
+    
+    //Withdraw/deposit Ether//
+    
+    function depositEther() payable{
+        
+    }
+    
+    function withdrawEther(uint amountInWei){
+        
+    }
+    
+    function getEthBalanceInWei() constant returns (uint) {
+        
+    }
+    
+    //Token Management//
+    
+    function addToken(string symbolName, address erc20TokenAddress){
+        require(!hasToken(symbolName));
+        symbolNameIndex++;
+        tokens[symbolNameIndex].symbolName = symbolName;
+        tokens[symbolNameIndex].tokenContract = erc20TokenAddress;
+    }
+    
+    function hasToken(string symbolName) constant returns (bool) {
+        uint8 index = getSymbolIndexName(symbolName);
+        if(index == 0){
+            return false;
+        }
+        return true;
+        
+    }
+    
+    function getSymbolIndexName(string symbolName) internal returns (uint8) {
+        for(uint8 i = 1; i <= symbolNameIndex; i++ ){
+            if(stringsEqual(tokens[i].symbolName, symbolName)){
+                return i;
+            }
+        }
+        return 0;
+    }
+    
+    //imported String comparison function//
+    
+    function stringsEqual(string storage _a, string memory _b) internal returns(bool){
+        bytes storage a = bytes(_a);
+        bytes memory b = bytes(_b);
+        if(a.length != b.length)
+            return false;
+            
+        for(uint i = 0; i < a.length; i++)
+            if(a[i] != b[i])
+                return false;
+        return true;       
+    }
+    
+    //get order books//
+    
+    function getBuyOrderBook(string symbolName) constant returns (uint[], uint[]) {
+        
+    }
+    
+    function getSellOrderBook(string symbolName) constant returns (uint[], uint[]){
+        
+    }
+    
+    //Bid order//
+    
+    function buyToken(string symbolName, uint priceInWei, uint amount){
+        
+    }
+    
+    //Ask Order//
+    
+    function sellToken(string symbolName, uint priceInWei, uint amount){
+        
+    }
+    
+    //Cancel Order//
+    
+    function cancelOrder(string symbolName, bool isSellOrder, uint priceInWei, uint offerKey){
+        
+    }
+    
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var fixedSupplyToken = artifacts.require("./FixedSupplyToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(fixedSupplyToken);
 };
